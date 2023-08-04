14c130226d23e33d915645a501653af296049fc3
julius caezar <julius.caezar25@gmail.com>
2023-08-03 23:56:27 -0400

    feat: criando estilização para a description e image

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,5 +4,16 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFF'
+  },
+  description: {
+    color: '#2E9D4C',
+    textAlign: 'center',
+    fontFamily: 'Poppins_400Regular',
+    fontSize: 14,
+    flex: 1,
+    textAlignVertical: 'center'
+  },
+  image: {
+    flex: 1
   }
 })
