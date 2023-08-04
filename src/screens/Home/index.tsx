70637c5c3a757f15dc1e400b4fe4bb91527235ae
julius caezar <julius.caezar25@gmail.com>
2023-08-03 23:39:30 -0400

    feat: commit inicial

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { styles } from "./styles";
+import Button from "../../components/Button";
+
+export default function index() {
+  return (
+    <View style={styles.container}>
+      <Button />
+    </View>
+  )
+}
