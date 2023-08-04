1666fb6010648d3dd7ca05543cfbda7be1a44f8b
julius caezar <julius.caezar25@gmail.com>
2023-08-03 23:58:34 -0400

    feat: importando o hook useState, criando a função assincrona handleSelectImage e criando os componentes de images e text

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,31 @@
-import { View } from "react-native";
+import { View, Image, Text } from "react-native";
 import { styles } from "./styles";
 import Button from "../../components/Button";
+import { useState } from "react";
 
-export default function index() {
+export default function Home() {
+  const [selectedImageUri, setselectedImageUri] = useState('');
+
+  async function handleSelectImage() {
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Button />
+      <Button onPress={handleSelectImage}/>
+
+      {
+        selectedImageUri ?
+        <Image 
+          source={{ uri: selectedImageUri }} 
+          style={styles.image}
+          resizeMode="cover"
+        />
+        : 
+        <Text style={styles.description}>
+          Selecione a foto do seu prato para analizar
+        </Text>
+      }
     </View>
   )
 }
