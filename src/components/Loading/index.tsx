5659886b7d1e789488d48d47cdc122a1558e797d
julius caezar <julius.caezar25@gmail.com>
2023-08-04 10:08:12 -0400

    feat: criando o component de Loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import { ActivityIndicator, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View>
+      <ActivityIndicator color='#2E9D4C'/>
+    </View>
+  )
+}
